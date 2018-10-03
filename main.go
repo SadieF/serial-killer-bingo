936cab664a6e3ee6948086f9ac24614b076729a0
SadieF <sadiefreeman@gmail.com>
2018-10-02 20:07:46 -0700

    Swapped out bingo into types folder - doesn't work yet

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
@@ -32,62 +33,27 @@ func main() {
 	router.Run(":3000")
 }
 
-//Bingo containes information about a single serial killer trait
-type Bingo struct {
-	ID int `json:"id" binding:"required"`
-	Marked bool `json:"marked"`
-	Trait string `json:"trait" binding:"required"`
-}
-
-//List of traits
-var Bingo = []Bingo{
-	Bingo{1, false, "Drives a van"},
-	Bingo{1, false, "Arson"},
-	Bingo{1, false, "Childhood Head Injury"},
-	Bingo{1, false, "Abandonment"},
-	Bingo{1, false, "Mother a prostitute"},
-	Bingo{1, false, "Bunker"},
-	Bingo{1, false, "Someone is in foster care"},
-	Bingo{1, false, "Cannibalism"},
-	Bingo{1, false, "Caucasian"},
-	Bingo{1, false, "Tortured or killed pets"
-	Bingo{1, false, "Weak or absent father"},
-	Bingo{1, false, "FREE or Prison Break"},
-	Bingo{1, false, "Someone has a missing limb"},
-	Bingo{1, false, "Bedwetting"},
-	Bingo{1, false, "Sexual Deviance"},
-	Bingo{1, false, "Three first names"},
-	Bingo{1, false, "Dancing"},
-	Bingo{1, false, "Religious fanatics"},
-	Bingo{1, false, "Oddly specific preferences"},
-	Bingo{1, false, "Broom as a weapon"},
-	Bingo{1, false, "Two in one night"},
-	Bingo{1, false, "Mommy issues"},
-	Bingo{1, false, "High IQ"},
-	Bingo{1, false, "Catchy nickname"},
-}
-
-func main() {
-	// Leave this untouched
-}
-
 // BingoHandler returns the bingo card of serial killer traits
 func BingoHandler(c *gin.Context) {
 	c.Header("Content-type", "application/json")
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Bingo handler not implemented yet",
-	})
+	c.JSON(http.StatusOK, Bingos)
 }
 
 // MarkBingo marks the trait of a serial killer
 func MarkBingo(c *gin.Context) {
-	c.Header("Content-Type", "application/json")
-	c.JSON(http.StatusOK, gin.H{
-		"message": "MarkBingo handler not implemented yet",
-	})
-}
-
-
-
-
+	// Confirms bingo id is valid
+	if bingoid, err := strconv.Atoi(c.Param("bingoID")); err == nil {
+		for i := 0; i < len(Bingos); i++ {
+			if Bingos[i].ID == bingoid {
+				if Bingos[i].Marked == false {
+					Bingos[i].Marked = true
+				} else {
+					Bingos[i].Marked = false
+				}
+			}
+		}
+		c.JSON(http.StatusOK, &Bingos)
+	} else {
+		c.AbortWithStatus(http.StatusNotFound)
+	}
 }
